7da92680b8efbd5cfffeda76a21f9fc0f35a6893
jamshidbecky <jamshidbekmac@gmail.com>
2023-06-10 11:45:30 +0500

    stringni o'rtasiga boshqa string qo'shish

--- main.go
+++ main.go
@@ -2,25 +2,36 @@ package main
 
 import (
 	"fmt"
-	"strconv"
 )
 
-func main() {
-	var number int
-	var qoldiq, revNumber string
-	fmt.Scanln(&number)
+// func main() {
+// 	var number int
+// 	var qoldiq, revNumber string
+// 	fmt.Scanln(&number)
 
-	for number > 0 {
-		count := 0
-		qoldiq1 := number % 10
-		number = number / 10
-		qoldiq = strconv.Itoa(qoldiq1)
-		revNumber += qoldiq
-		count += 1
-	}
+// 	for number > 0 {
+// 		count := 0
+// 		qoldiq1 := number % 10
+// 		number = number / 10
+// 		qoldiq = strconv.Itoa(qoldiq1)
+// 		revNumber += qoldiq
+// 		count += 1
+// 	}
 
-	revNumber1, _ := strconv.Atoi(revNumber)
+// 	revNumber1, _ := strconv.Atoi(revNumber)
 
-	fmt.Println(revNumber1)
+// 	fmt.Println(revNumber1)
 	
+// }
+
+func main() {
+	var str1, str2 string
+
+	fmt.Scanln(&str1)
+	fmt.Scanln(&str2)
+
+	res := str1[:len(str1) / 2] + str2 + str1[:len(str1) / 2]
+
+	fmt.Println(res)
+
 }
