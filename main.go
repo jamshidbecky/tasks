f2f7b4b3494bd29c8348b7461bcd582d7d838482
jamshidbecky <jamshidbekmac@gmail.com>
2023-06-10 11:23:44 +0500

    ixtiyoriy sonni teskari tartibda yozish

--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
-func main()  {
+func main() {
+	var number int
+	var qoldiq, revNumber string
+	fmt.Scanln(&number)
 
+	for number > 0 {
+		count := 0
+		qoldiq1 := number % 10
+		number = number / 10
+		qoldiq = strconv.Itoa(qoldiq1)
+		revNumber += qoldiq
+		count += 1
+	}
+
+	revNumber1, _ := strconv.Atoi(revNumber)
+
+	fmt.Println(revNumber1)
+	
 }
